af6b8d28802178a88aa5176db0147e297ee626a9
Nicolás Rodríguez <18080513+nico-rodriguez@users.noreply.github.com>
2021-09-04 00:05:30 -0300

    Mongoose ODM Part 2

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -9,29 +9,46 @@ const connect = mongoose.connect(URL);
 connect.then((db) => {
   console.log('Connected to server!');
 
-  let newDish = Dishes({
+  Dishes.create({
     name: 'Uthapizza',
     description: 'test'
-  });
+  })
+  .then((dish) => {
+    console.log('Created dish:');
+    console.log(dish);
 
-  newDish.save()
-    .then((dish) => {
-      console.log('Saved dish');
-      console.log(dish);
+    console.log('Updating dish...');
 
-      return Dishes.find({}).exec();
-    })
-    .then((dishes) => {
-      console.log('Dishes collection:');
-      console.log(dishes);
+    return Dishes.findByIdAndUpdate(dish._id, {
+      $set: { description: 'Updated test' }
+    }, {
+      new: true
+    }).exec();
+  })
+  .then((dish) => {
+    console.log('Updated dish:');
+    console.log(dish);
 
-      return Dishes.deleteMany({});
-    })
-    .then(() => {
-      console.log('Closing connection to server...');
-      return mongoose.connection.close();
-    })
-    .catch((err) => {
-      console.error(err);
+    dish.comments.push({
+      rating: 5,
+      comment: 'I\'m getting a sinking feeling!',
+      author: 'Leonardo di Carpaccio'
     });
+
+    return dish.save();
+  })
+  .then((dish) => {
+    console.log('Updated dish comments:');
+    console.log(dish);
+
+    console.log('Dropping Dishes...');
+    return Dishes.deleteMany({});
+  })
+  .then(() => {
+    console.log('Closing connection to server...');
+    return mongoose.connection.close();
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 });
