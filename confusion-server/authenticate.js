fd4ba55b12514a649c40e754876f0d3c73121a3f
Nicolás Rodríguez <18080513+nico-rodriguez@users.noreply.github.com>
2021-09-14 12:46:46 -0300

    User Authentication with Passport

---
+++ confusion-server/authenticate.js
@@ -0,0 +1,7 @@
+var passport = require('passport');
+var User = require('./models/user');
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
