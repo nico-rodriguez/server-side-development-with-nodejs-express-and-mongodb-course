ffaf20b33dfb0c2a998463afc67963f152825223
Nicolás Rodríguez <18080513+nico-rodriguez@users.noreply.github.com>
2021-09-18 20:53:19 -0300

    Uploading Files

--- confusion-server/app.js
+++ confusion-server/app.js
@@ -13,6 +13,7 @@ const usersRouter = require('./routes/users');
 const dishRouter = require('./routes/dishRouter');
 const leaderRouter = require('./routes/leaderRouter');
 const promoRouter = require('./routes/promoRouter');
+const uploadRouter = require('./routes/uploadRouter');
 
 const mongoose = require('mongoose');
 
@@ -54,6 +55,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes', dishRouter);
 app.use('/leaders', leaderRouter);
 app.use('/promotions', promoRouter);
+app.use('/imageUpload', uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
